5adeb3ca603dde049ffec42f8c9dccfca2bcd37d
João Bispo <vitor.vi10@hotmail.com>
2020-09-11 23:13:06 -0300

    :rocket: feat: connect to mysql database

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const customExpress = require("./src/config/customExpress");
+const connection = require('./src/app/database/connection');
 
-const app = customExpress();
 
-app.listen(3000, (req, res) => {
-    console.log('Servidor Rodando na porta 3000')
-});
+connection.connect((err) => {
+    if (err) {
+        console.log(err);
+
+    } else {
+
+        console.log('Conectado com sucesso!');
+
+        const app = customExpress();
+
+        app.listen(3000, (req, res) => {
+            console.log('Servidor Rodando na porta 3000')
+        });
+
+    }
+})
--- src/app/controllers/atendimentos.js
+++ src/app/controllers/atendimentos.js
@@ -1,5 +1,11 @@
 module.exports = (app) => {
     app.get('/atendimentos', (req, res) => {
-        res.send('você está na rota de atendimentos');
+        res.send('você está na rota de atendimentos e está realizando um GET');
     });
+
+    app.post('/atendimentos', (req, res) => {
+        console.log(req.body);
+        res.send('Você está rota de  atendimentos e está realizando um POST')
+    });
+
 }
