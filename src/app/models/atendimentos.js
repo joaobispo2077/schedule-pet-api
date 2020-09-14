730195e43eb826f34d3efa0cf0e601ad91ba91f0
João Bispo <vitor.vi10@hotmail.com>
2020-09-14 15:51:44 -0300

    :rocket: feat: validations

--- src/app/models/atendimentos.js
+++ src/app/models/atendimentos.js
@@ -7,17 +7,42 @@ class Atendimentos {
         const dataCriacao = moment().format('YYYY-MM-DD HH:mm:ss');
         const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:mm:ss')
 
-        const atendimentoDatado = {...atendimento, dataCriacao, data }
+        const isValidDate = moment(data).isSameOrAfter(dataCriacao);
+        const isValidName = atendimento.cliente.length >= 4;
 
-        const sql = 'INSERT INTO Atendimentos SET ?';
+        const validations = [
 
-        connection.query(sql, atendimentoDatado, (err, result) => {
-            if (err) {
-                res.status(400).json(err); //400 Bad Request
-            } else {
-                res.status(201).json(result); // 201 Created w Success
+            {
+                nome: 'data',
+                valido: isValidDate,
+                mensagem: 'A data deve ser maior ou igual que a data atual'
+            },
+            {
+                nome: 'nome',
+                valido: isValidName,
+                mensagem: 'O nome do cliente deve ter pelo menos 4 caracteres'
             }
-        });
+        ];
+
+        const errors = validations.filter((validation) => !validation.valido);
+        const areThereErrors = errors.length;
+
+        if (areThereErrors) {
+            res.status(400).json(errors);
+        } else {
+            const atendimentoDatado = {...atendimento, dataCriacao, data }
+
+            const sql = 'INSERT INTO Atendimentos SET ?';
+
+            connection.query(sql, atendimentoDatado, (err, result) => {
+                if (err || !isValidName, !isValidDate) {
+                    res.status(400).json(err); //400 Bad Request
+                } else {
+                    res.status(201).json(result); // 201 Created w Success
+                }
+            });
+        }
+
     }
 }
 
