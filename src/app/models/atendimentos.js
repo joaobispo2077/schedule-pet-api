f186bc0b396997846efb22b45e639f4e19690ea1
João Bispo <vitor.vi10@hotmail.com>
2020-09-14 15:06:02 -0300

    :thought_balloon: refactor: dates on moment.js

--- src/app/models/atendimentos.js
+++ src/app/models/atendimentos.js
@@ -1,11 +1,17 @@
+const moment = require('moment');
+
 const connection = require('../infra/connection');
 class Atendimentos {
 
     create(atendimento) {
+        const dataCriacao = moment().format('YYYY-MM-DD HH:mm:ss');
+        const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:mm:ss')
+
+        const atendimentoDatado = {...atendimento, dataCriacao, data }
 
         const sql = 'INSERT INTO Atendimentos SET ?';
 
-        connection.query(sql, atendimento, (err, result) => {
+        connection.query(sql, atendimentoDatado, (err, result) => {
             if (err) {
                 console.log(err);
             } else {
